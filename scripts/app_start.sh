842d4a02167a2c7fe48a3a424cf57ae5ab036150
neerajallen <100480612+neerajallen@users.noreply.github.com>
2023-02-17 15:13:12 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd /home/ubuntu
-npm start
-pm2 start npm --name "covidapp" -- start
-pm2 startup
+#npm start
+#pm2 start npm --name "covidapp" -- start
+#pm2 startup 
+pm2 serve build 3000 --spa
+
 pm2 save
-pm2 restart all
+#pm2 restart all
